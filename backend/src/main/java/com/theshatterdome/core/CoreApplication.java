d2ff08a954ee9e7a1d75c4f58cd6fe243004b4b9
sydneyafternoon <sydneyafternoon@gmail.com>
2025-08-08 20:03:22 +0800

    build: remove CORS limit

--- backend/src/main/java/com/theshatterdome/core/CoreApplication.java
+++ backend/src/main/java/com/theshatterdome/core/CoreApplication.java
@@ -3,11 +3,27 @@ package com.theshatterdome.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// CORS imports
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @SpringBootApplication
 public class CoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoreApplication.class, args);
+	}
+
+	// Enable CORS for requests from the React frontend
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
 	}
 
 }
