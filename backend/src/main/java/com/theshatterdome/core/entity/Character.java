21589db6ced37f4f4228349e48c6b5128780e6c6
sydneyafternoon <sydneyafternoon@gmail.com>
2025-08-17 19:32:29 +0800

    refactor: reflect the removal of attributes in mysql table

--- backend/src/main/java/com/theshatterdome/core/entity/Character.java
+++ backend/src/main/java/com/theshatterdome/core/entity/Character.java
@@ -12,14 +12,8 @@ public class Character {
     private String name;
     @Column(name = "class")
     private String characterClass;
-    private int level;
     private int health;
     private int dexterity;
-    private int strength;
-    private int wisdom;
-    private int wickedness;
-    private int acuity;
-    private int faith;
 
     // Getters and setters
     public int getId() { return id; }
@@ -31,27 +25,9 @@ public class Character {
     public String getCharacterClass() { return characterClass; }
     public void setClass(String className) { this.characterClass = className; }
 
-    public int getLevel() { return level; }
-    public void setLevel(int level) { this.level = level; }
-
     public int getHealth() { return health; }
     public void setHealth(int health) { this.health = health; }
 
     public int getDexterity() { return dexterity; }
     public void setDexterity(int dexterity) { this.dexterity = dexterity; }
-
-    public int getStrength() { return strength; }
-    public void setStrength(int strength) { this.strength = strength; }
-
-    public int getWisdom() { return wisdom; }
-    public void setWisdom(int wisdom) { this.wisdom = wisdom; }
-
-    public int getWickedness() { return wickedness; }
-    public void setWickedness(int wickedness) { this.wickedness = wickedness; }
-
-    public int getAcuity() { return acuity; }
-    public void setAcuity(int acuity) { this.acuity = acuity; }
-
-    public int getFaith() { return faith; }
-    public void setFaith(int faith) { this.faith = faith; }
 }
