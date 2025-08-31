fe165312581ca00eb0354bd478f4124226ee99a8
sydneyafternoon <sydneyafternoon@gmail.com>
2025-08-31 08:49:19 +0800

    build: revise import alias setting to enable vite recognize at mark imports

--- vite-frontend/vite.config.js
+++ vite-frontend/vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
+const srcPath = path.resolve(new URL('.', import.meta.url).pathname, "src");
+
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": srcPath,
+    },
+  },
   plugins: [react(), tailwindcss()],
-})
+});
